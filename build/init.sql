d1bcb83b9d43530422080ad7a92449336731bdc5
CI System <gordonbeeming@outlook.com>
2021-06-22 05:58:31 +0200

    should run init sql

--- build/init.sql
+++ build/init.sql
@@ -1,14 +1,14 @@
-IF NOT EXISTS(SELECT 1 FROM [solid-tribble].[dbo].[tb_Contacts])
+IF NOT EXISTS(SELECT 1 FROM [dbo].[tb_Contacts])
 BEGIN
-  INSERT INTO [solid-tribble].[dbo].[tb_Contacts] ([FirstName],[LastName],[EmailAddress]) 
+  INSERT INTO [dbo].[tb_Contacts] ([FirstName],[LastName],[EmailAddress]) 
   VALUES ('Gordon','Beeming','github@beeming.co.za')
 
-  INSERT INTO [solid-tribble].[dbo].[tb_Contacts] ([FirstName],[LastName],[EmailAddress]) 
+  INSERT INTO [dbo].[tb_Contacts] ([FirstName],[LastName],[EmailAddress]) 
   VALUES ('Bob','Jones','bobby@beeming.co.za')
 
-  INSERT INTO [solid-tribble].[dbo].[tb_Contacts] ([FirstName],[LastName],[EmailAddress]) 
+  INSERT INTO [dbo].[tb_Contacts] ([FirstName],[LastName],[EmailAddress]) 
   VALUES ('John','Doe','john@beeming.co.za')
 
-  INSERT INTO [solid-tribble].[dbo].[tb_Contacts] ([FirstName],[LastName],[EmailAddress]) 
+  INSERT INTO [dbo].[tb_Contacts] ([FirstName],[LastName],[EmailAddress]) 
   VALUES ('Mary','','mary@beeming.co.za')
 END
